6f64d0298fb978d9efd994a454425dc4bb63ac89
Angel Morales <angelm@ubits.co>
2024-07-08 11:22:22 -0500

    clean

--- payment-processing-dashboard/src/main/java/org/dev/paymentprocessingdashboard/PaymentProcessingDashboardApplication.java
+++ payment-processing-dashboard/src/main/java/org/dev/paymentprocessingdashboard/PaymentProcessingDashboardApplication.java
@@ -4,16 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 @SpringBootApplication
 @EnableScheduling
 public class PaymentProcessingDashboardApplication {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(PaymentProcessingDashboardApplication.class, args);
         System.out.println("Payment Processing Dashboard is running...");
     }
