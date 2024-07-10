38b581b9bff9fa379ec8245f5f27186b54f53c9a
Angel Morales <angelm@ubits.co>
2024-07-10 17:36:52 -0500

    validation for units test

--- payment-processing-dashboard/src/main/java/org/dev/paymentprocessingdashboard/infraestructure/adapter/in/FileReaderAdapter.java
+++ payment-processing-dashboard/src/main/java/org/dev/paymentprocessingdashboard/infraestructure/adapter/in/FileReaderAdapter.java
@@ -16,7 +16,19 @@ public class FileReaderAdapter implements IFileReaderPort {
     private static final String LOG_FILE_PATH = "log-generator/logs/transactions.log";
     private static final String POSITION_FILE_PATH = "payment-processing-dashboard/last_known_position.txt";
     private final Path path = Paths.get(LOG_FILE_PATH);
-    private final Path positionFilePath = Paths.get(POSITION_FILE_PATH);
+    //private final Path positionFilePath = Paths.get(POSITION_FILE_PATH);
+    private static Path positionFilePath;
+
+    // This static block is used to determine the positionFilePath based on the user.dir system property
+    // validation for unit tests
+    static {
+        String userDir = System.getProperty("user.dir");
+        if (userDir.endsWith("payment-processing-dashboard")) {
+            positionFilePath = Paths.get(userDir, "last_known_position.txt");
+        } else {
+            positionFilePath = Paths.get(userDir, POSITION_FILE_PATH);
+        }
+    }
     private long lastKnownPosition = 0;
 
     public FileReaderAdapter() throws IOException {
