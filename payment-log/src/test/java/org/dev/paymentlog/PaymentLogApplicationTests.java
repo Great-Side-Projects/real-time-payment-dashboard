75591f5b2e70132746ced86fb92681344aa8127b
Angel Morales <angelm@ubits.co>
2024-08-13 16:17:23 -0500

    payment-log initial test

--- payment-log/src/test/java/org/dev/paymentlog/PaymentLogApplicationTests.java
+++ payment-log/src/test/java/org/dev/paymentlog/PaymentLogApplicationTests.java
@@ -2,14 +2,19 @@ package org.dev.paymentlog;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class PaymentLogApplicationTests {
 
+    @Autowired
+    private ApplicationContext context;
     @Test
-    @DisplayName("Application context loads successfully")
     void applicationContextLoadsSuccessfully() {
-        PaymentLogApplication.main(new String[]{});
+        assertThat(context).isNotNull();
     }
 }
