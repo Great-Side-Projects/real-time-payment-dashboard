bd6979344e5ca3716da09a22b1b2c00a26dd7f34
Angel Morales <angelm@ubits.co>
2024-08-15 21:30:30 -0500

    refactor separation of concern

--- payment-notification-hubs/src/test/java/org/dev/paymentnotificationhubs/PaymentNotificationHubsApplicationTests.java
+++ payment-notification-hubs/src/test/java/org/dev/paymentnotificationhubs/PaymentNotificationHubsApplicationTests.java
@@ -1,13 +1,19 @@
 package org.dev.paymentnotificationhubs;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class PaymentNotificationHubsApplicationTests {
 
+    @Autowired
+    private ApplicationContext context;
     @Test
-    void contextLoads() {
+    void applicationContextLoadsSuccessfully() {
+        assertThat(context).isNotNull();
     }
-
 }
