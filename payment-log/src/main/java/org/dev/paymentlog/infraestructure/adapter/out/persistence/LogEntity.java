8adb2ace409d081e68e32b1957a584dbd116a8f8
Angel Morales <angelm@ubits.co>
2024-09-03 18:18:20 -0500

    index and UTF time

--- payment-log/src/main/java/org/dev/paymentlog/infraestructure/adapter/out/persistence/LogEntity.java
+++ payment-log/src/main/java/org/dev/paymentlog/infraestructure/adapter/out/persistence/LogEntity.java
@@ -2,19 +2,26 @@ package org.dev.paymentlog.infraestructure.adapter.out.persistence;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.UUID;
 
 @Setter
 @Getter
+//create index log_created_at on log(created_at);
 @Entity(name = "log")
+@Table(indexes = {
+        @Index(name = "idx_created_at", columnList = "created_at")
+})
 public class LogEntity {
 
     @Id
     private String id = UUID.randomUUID().toString();
     private String action;
     private String details;
-    private LocalDateTime createdAt = LocalDateTime.now();
+    private LocalDateTime createdAt = LocalDateTime.now(ZoneOffset.UTC);
 }
