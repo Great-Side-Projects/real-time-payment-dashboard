992270434f0819d9a6701bb27898e8f0f8f2feb5
Angel Morales <angelm@ubits.co>
2024-09-06 22:15:42 -0500

    deploy

--- payment-ingestion/src/main/java/org/dev/paymentingestion/PaymentIngestionApplication.java
+++ payment-ingestion/src/main/java/org/dev/paymentingestion/PaymentIngestionApplication.java
@@ -10,6 +10,6 @@ public class PaymentIngestionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentIngestionApplication.class, args);
-		System.out.println("Payment Ingestion Application started...");
+		System.out.println("Payment Ingestion Application started...-");
 	}
 }
