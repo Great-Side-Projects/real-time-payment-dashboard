68de036adacb622cb60cdbb57cc33313ca2995c0
Angel Morales <angelm@ubits.co>
2024-09-12 23:18:07 -0500

    deploy websocket

--- payment-websocket-notification/src/main/java/org/dev/paymentwebsocketnotification/infraestructure/WebSocketConfig.java
+++ payment-websocket-notification/src/main/java/org/dev/paymentwebsocketnotification/infraestructure/WebSocketConfig.java
@@ -18,6 +18,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws");
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 }
