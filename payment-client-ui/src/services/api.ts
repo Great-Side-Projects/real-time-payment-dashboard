f9e7af9352dd9f670fffdde78de6a27f5d77ae72
Angel Morales <angelm@ubits.co>
2024-09-27 17:08:43 -0500

    separation environment variables

--- payment-client-ui/src/services/api.ts
+++ payment-client-ui/src/services/api.ts
@@ -2,23 +2,11 @@ import { Transaction } from '../types'
 import axios from 'axios';
 
 
-const API_BASE_PROCESSIN_URL = `${process.env.NEXT_PUBLIC_PAYMENT_URL}/processing/transactions`;
-const API_BASE_INGESTION_URL = `${process.env.NEXT_PUBLIC_PAYMENT_URL}/ingestion/transactions`;
-
-
-/*
-export const sendTransaction = async (transaction: Transaction[]): Promise<void> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_PAYMENT_INGESTION_URL}/transactions/process`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(transaction)
-  })
-  if (!response.ok) throw new Error('Failed to process transaction')
-}
-*/
+const API_BASE_PROCESSING_URL = `${process.env.NEXT_PUBLIC_PAYMENT_URL}/transactions/filter`;
+const API_BASE_INGESTION_URL = `${process.env.NEXT_PUBLIC_PAYMENT_URL}/transactions/process`;
 
 export const sendTransaction = async (transactions: Transaction[]) => {
-  const response = await axios.post(`${API_BASE_INGESTION_URL}/process`, transactions);
+  const response = await axios.post(`${API_BASE_INGESTION_URL}`, transactions);
   return response.data;
 };
 
@@ -28,6 +16,6 @@ interface TransactionParams {
 }
 
 export const getTransactions = async (params: TransactionParams) => {
-  const response = await axios.get(`${API_BASE_PROCESSIN_URL}/filter`, { params });
+  const response = await axios.get(`${API_BASE_PROCESSING_URL}`, { params });
   return response.data;
 };
