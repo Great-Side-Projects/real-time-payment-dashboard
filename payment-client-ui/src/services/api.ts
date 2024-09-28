06116257eaa0625181866b1b4f925f89884725be
Angel Morales <angelm@ubits.co>
2024-09-28 10:12:59 -0500

    separation environment variables

--- payment-client-ui/src/services/api.ts
+++ payment-client-ui/src/services/api.ts
@@ -2,10 +2,11 @@ import { Transaction } from '../types'
 import axios from 'axios';
 
 
-const API_BASE_PROCESSING_URL = `${process.env.NEXT_PUBLIC_PAYMENT_URL}/transactions/filter`;
-const API_BASE_INGESTION_URL = `${process.env.NEXT_PUBLIC_PAYMENT_URL}/transactions/process`;
+const API_BASE_INGESTION_URL = `${process.env.NEXT_PUBLIC_PAYMENT_INGESTION_URL}/transactions/process`;
+const API_BASE_PROCESSING_URL = `${process.env.NEXT_PUBLIC_PAYMENT_PROCESSING_URL}/transactions/filter`;
 
 export const sendTransaction = async (transactions: Transaction[]) => {
+  console.log('API_BASE_PROCESSING_URL ', API_BASE_INGESTION_URL);
   const response = await axios.post(`${API_BASE_INGESTION_URL}`, transactions);
   return response.data;
 };
@@ -16,6 +17,7 @@ interface TransactionParams {
 }
 
 export const getTransactions = async (params: TransactionParams) => {
+  console.log('API_BASE_PROCESSING_URL ', API_BASE_PROCESSING_URL);
   const response = await axios.get(`${API_BASE_PROCESSING_URL}`, { params });
   return response.data;
 };
